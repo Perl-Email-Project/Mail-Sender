7f33e16ff45dceb9c897613b5a7b99b82874b160
Chase Whitener <cwhitener@gmail.com>
2016-05-16 21:05:00 -0400

    Revert the Win32 lookup to the original-ish way so we die on error

--- lib/Mail/Sender/CType/Win32.pm
+++ lib/Mail/Sender/CType/Win32.pm
@@ -3,32 +3,50 @@ package Mail::Sender::CType::Win32;
 use strict;
 use warnings;
 use Mail::Sender ();
+use Win32API::Registry qw(RegOpenKeyEx KEY_READ HKEY_CLASSES_ROOT RegQueryValueEx);
 
-my $error = do {
-    local $@;
-    eval {
-        require Win32API::Registry;
-    };
-    $@;
-};
+no warnings 'redefine';
 
-
-unless ( $error ) {
-    no strict 'subs';
-    no warnings 'redefine';
-    import Win32API::Registry qw(RegOpenKeyEx KEY_READ HKEY_CLASSES_ROOT RegQueryValueEx);
-
-    # replace the GuessCType sub with this one!
-    *Mail::Sender::GuessCType = sub {
-        my $ext = shift;
-        $ext =~ s/^.*\././;
-        my ($key, $type, $data);
-        RegOpenKeyEx(HKEY_CLASSES_ROOT, $ext, 0, KEY_READ, $key)
-            or return 'application/octet-stream';
-        RegQueryValueEx($key, "Content Type", [], $type, $data, [])
-            or return 'application/octet-stream';
-        return $data || 'application/octet-stream';
-    };
+*Mail::Sender::GuessCType = \&GuessCType;
+sub GuessCType {
+    my $ext = shift;
+    $ext =~ s/^.*\././;
+    my ($key, $type, $data);
+    RegOpenKeyEx(HKEY_CLASSES_ROOT, $ext, 0, KEY_READ, $key)
+        or return 'application/octet-stream';
+    RegQueryValueEx($key, "Content Type", [], $type, $data, [])
+        or return 'application/octet-stream';
+    return $data || 'application/octet-stream';
 }
 
 1;
+__END__
+
+=encoding UTF-8
+
+=head1 NAME
+
+Mail::Sender::CType::Win32 - Alter how we find the MIME type of a file.
+
+=head1 SYNOPSIS
+
+    use Mail::Sender;
+    use Mail::Sender::CType::Win32;
+
+    # use the registry to find the MIME Type
+    my $type = Mail::Sender::GuessCType("foo.txt");
+
+=head1 DESCRIPTION
+
+This exists solely to change the way you look up MIME types in Windows environments.
+
+=head1 FUNCTIONS
+
+L<Mail::Sender::CType::Win32> makes the following functions available and overrides
+the functions in L<Mail::Sender> with these functions as well.
+
+=head2 GuessCType
+
+Guess the MIME type using the Windows registry.
+
+=cut
